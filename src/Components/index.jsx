60f0db3f64b60c888ed5079a064b4ff67747254d
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-04-27 16:05:24 -0300

    fix

---
+++ src/Components/index.jsx
@@ -0,0 +1,198 @@
+import { RiMailLine, RiDualSim2Fill } from "react-icons/ri";
+import { GiProcessor } from "react-icons/gi";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <section
+        style={{
+          marginTop: "60px",
+
+          width: "50%",
+          height: "500px",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            width: " 100%",
+            textAlign: "center",
+            cursor: "pointer",
+            marginTop: "50px",
+            marginLeft: "30px",
+          }}
+        >
+          <b style={{ marginTop: "20px" }}>Document</b>
+          <RiDualSim2Fill
+            style={{
+              fontSize: "50px",
+              position: "relative",
+              top: "-35px",
+              left: "10px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            width: " 100%",
+            textAlign: "center",
+            marginTop: "50px",
+            cursor: "pointer",
+          }}
+        >
+          <b style={{ marginTop: "20px" }}>Email</b>
+          <RiMailLine
+            style={{
+              fontSize: "50px",
+              position: "relative",
+              top: "-35px",
+              left: "20px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            width: " 100%",
+            textAlign: "center",
+            marginTop: "50px",
+            cursor: "pointer",
+          }}
+        >
+          <b style={{ marginTop: "20px" }}>Proyect Process</b>
+          <GiProcessor
+            style={{
+              fontSize: "50px",
+              position: "relative",
+              top: "-35px",
+            }}
+          />
+        </div>
+
+        <section
+          style={{
+            width: "39%",
+            top: "40%",
+            height: "50%",
+            position: "absolute",
+            border: "1px solid black",
+            borderBottom: "none",
+            overflow: "hidden",
+            overflowY: "scroll",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              marginLeft: "-10px",
+              marginTop: "30px",
+            }}
+          >
+            <b style={{ marginLeft: "50px" }}>Type</b>
+            <b style={{ marginLeft: "150px" }}> State</b>
+            <b style={{ marginLeft: "150px" }}> Process</b>
+            <b style={{ marginLeft: "120px" }}> Type Contract</b>
+          </div>
+
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}>Undefined</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Client</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}>months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Client</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+          <div
+            style={{ display: "flex", marginTop: "30px", marginLeft: "-10px" }}
+          >
+            <h3 style={{ marginLeft: "50px" }}>Users</h3>
+            <h3 style={{ marginLeft: "150px" }}>Check In</h3>
+            <h3 style={{ marginLeft: "150px" }}>40%</h3>
+            <h3 style={{ marginLeft: "150px" }}> 12 months</h3>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default App;
